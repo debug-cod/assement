cb5092bfe7c5965ab2b65324d7b6badf2bee1424
Wang <72582032+debug-cod@users.noreply.github.com>
2025-10-08 23:04:36 +0100

    Add files via upload

---
+++ petWatch/controllers/login.php
@@ -0,0 +1,48 @@
+login.php
+<?php
+// Login controller for petWatch
+// This file handles user login
+
+// Start session to track login status
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Set page title
+$page_title = "Login - petWatch";
+
+// Check if form was submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get form data
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
+
+    // Simple user validation (for testing without database)
+    $valid_users = [
+        'A' => 'P@ssw0rd123!Xy', // Strong password for user A
+        'C' => '123456' // Weak password for user C
+    ];
+
+    // Check if username exists
+    if (array_key_exists($username, $valid_users)) {
+        // Check if password matches
+        if ($password === $valid_users[$username]) {
+            // Login successful
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $username;
+
+            // Redirect to home page
+            header("Location: ../index.php");
+            exit();
+        } else {
+            $error_message = "Wrong password. Please try again.";
+        }
+    } else {
+        $error_message = "Username not found.";
+    }
+}
+
+
+// Include login view
+include '../views/login.phtml';
+?>
--- petWatch/index.php
+++ petWatch/index.php
@@ -2,14 +2,20 @@
 // petWatch website - home page
 // This file shows the home page of petWatch
 
-// Start session for user login (will be used later)
+// Start session for user login
 session_start();
 
-// Set page title
+// Set page title variable
 $page_title = "petWatch - Find Missing Pets";
 
+// Check if user has weak password warning
+if (isset($_SESSION['password_weak']) && $_SESSION['password_weak']) {
+    // Show warning message (you can display this in the header or somewhere)
+    $show_password_warning = true;
+}
+
 // Include the home page view
-include 'views/template/home.phtml';
+include 'views/home.phtml';
 
 // End of index.php file
 ?>
